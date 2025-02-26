d786e580dacfe89a4e598004ca0657d7be0fe9d7
fschmid-dev <info@fschmid.dev>
2025-02-26 17:42:26 +0100

    Update base-url

--- vite.config.js
+++ vite.config.js
@@ -40,4 +40,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+  base: process.env.NODE_ENV === 'production' ? '/gothic-tales/' : '',
 })
