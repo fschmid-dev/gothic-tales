accd1d4bc1a8943f5e5c66c91f316e936b6e0c3b
FSchmid <info@fschmid.dev>
2025-07-11 14:47:16 +0200

    Updates

--- assets/libraries-BRcsaPPZ.js
+++ assets/libraries-BRcsaPPZ.js
@@ -1,4 +1,4 @@
-import{d as Zi,c as Ue,w as Qi,h as Ji}from"./vue-CLvJc0W0.js";/*!
+import{d as Zi,c as Ue,w as Qi,h as Ji}from"./vue-DrhbM47G.js";/*!
  * Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com
  * License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License)
  * Copyright 2024 Fonticons, Inc.
